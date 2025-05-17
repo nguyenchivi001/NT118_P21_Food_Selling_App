f144ba3f28fbd656d8927ff4e405015f8aaba78f
Sua Duong <22521267@gm.uit.edu.vn>
2025-05-17 17:14:12 +0700

    Refine UI

--- app/src/main/java/com/example/food_selling_app/ui/ProfileActivity.java
+++ app/src/main/java/com/example/food_selling_app/ui/ProfileActivity.java
@@ -2,6 +2,7 @@ package com.example.food_selling_app.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
