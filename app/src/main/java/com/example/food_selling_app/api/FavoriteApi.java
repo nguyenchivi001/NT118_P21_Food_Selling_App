318ab0dd8be8bbc61639be789fa7696debe69a70
nguyenchivi001 <nguyenchivi292004@gmail.com>
2025-05-18 16:35:55 +0700

    Fix UI and get data favorites from api and delete favoriteItem from favorite

--- app/src/main/java/com/example/food_selling_app/api/FavoriteApi.java
+++ app/src/main/java/com/example/food_selling_app/api/FavoriteApi.java
@@ -1,4 +1,20 @@
 package com.example.food_selling_app.api;
 
-public class FavoriteApi {
+import com.example.food_selling_app.dto.response.ApiResponse;
+import com.example.food_selling_app.model.FavoriteItem;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface FavoriteApi {
+
+    @GET("api/favorite-items")
+    Call<ApiResponse<List<FavoriteItem>>> getFavoriteItems();
+
+    @DELETE("api/favorite-items/{foodId}")
+    Call<ApiResponse<Void>> removeFromFavorite(@Path("foodId") Integer foodId);
 }
